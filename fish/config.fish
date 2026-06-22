388b1a6313507d03e528f486b0c635b6654d086c
absurdismforme <swymkhandelwal@gmail.com>
2026-06-22 13:12:11 +0530

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,24 @@
-/home/swayam/D/fish/config.fish
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Suppress the default greeting message
+set fish_greeting ""
+
+# Environment Variables (Fish uses 'set -gx' instead of 'export')
+set -gx EDITOR nano
+set -gx VISUAL nano
+
+# Aliases (Fish automatically turns these into clean functions under the hood)
+alias ls="ls --color=auto"
+alias ll="ls -la"
+alias g="git"
+alias ..="cd .."
+alias ...="cd ../.."
+
+starship init fish | source
+
+# ZVM
+set -gx ZVM_INSTALL "$HOME/.zvm/self"
+set -gx PATH $PATH "$HOME/.zvm/bin"
+set -gx PATH $PATH "$ZVM_INSTALL/"
